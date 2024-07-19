a14aa00a443792875452d4c52363891bc56c1aa2
Prabesh Paudel <poudelprabesh2277@gmail.com>
2024-07-19 16:28:23 +0545

    first commit

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const data = {
+    name: "MY_NAME",
+    githubLink: "https://github.com/Broitsliterallyme" ,
+    email : "MAIL_ADDRESS",
+    hobbies : [
+        "Movies",
+        "Travelling",
+        "Football"
+    ],
+    jobTitle : "Software Engineer",
+    degree: "Bachelor's in Computer Engineering",
+    campus: "Pulchowk Campus",
+    location: "Nepal",
+    spokenLanguages: "Nepali,English",
+}
+export default data;
